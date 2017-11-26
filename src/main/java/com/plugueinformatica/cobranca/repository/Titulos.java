d657a7eee210adedd4583251358f1845472fab5e
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-26 04:14:19 -0300

    Repositório do projeto

---
+++ src/main/java/com/plugueinformatica/cobranca/repository/Titulos.java
@@ -0,0 +1,11 @@
+package com.plugueinformatica.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plugueinformatica.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+	
+	
+
+}
